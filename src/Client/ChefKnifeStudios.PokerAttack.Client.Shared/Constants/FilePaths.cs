1ff1db92887e1f125f38bae6dc5baa10a93c82b8
Henry Faulkner <hfaulkner27@gmail.com>
2025-09-14 21:10:58 -0400

    Add some assets

---
+++ src/Client/ChefKnifeStudios.PokerAttack.Client.Shared/Constants/FilePaths.cs
@@ -0,0 +1,83 @@
+﻿namespace ChefKnifeStudios.PokerAttack.Client.Shared.Constants
+{
+    public sealed partial class FilePaths
+    {
+        const string PathToRoot = "_content/ChefKnifeStudios.PokerAttack.Client.Shared/";
+
+        const string PathToAudio = $"{PathToRoot}audio/";
+
+        public const string PlaceCardOne = $"{PathToAudio}cardPlace1";
+        public const string PlaceCardTwo = $"{PathToAudio}cardPlace2";
+        public const string PlaceCardThree = $"{PathToAudio}cardPlace3";
+        public const string SlideCardOne = $"{PathToAudio}cardSlide1";
+        public const string SlideCardTwo = $"{PathToAudio}cardSlide2";
+        public const string SlideCardThree = $"{PathToAudio}cardSlide3";
+        public const string CollideChipsOne = $"{PathToAudio}chipsCollide1";
+        public const string CollideChipsTwo = $"{PathToAudio}chipsCollide2";
+        public const string CollideChipsThree = $"{PathToAudio}chipsCollide3";
+
+        const string PathToImages = $"{PathToRoot}images/";
+
+        const string PathToCards = $"{PathToImages}cards/";
+
+        // Clubs
+        public const string AceOfClubs = $"{PathToCards}cardClubsA";
+        public const string TwoOfClubs = $"{PathToCards}cardClubs2";
+        public const string ThreeOfClubs = $"{PathToCards}cardClubs3";
+        public const string FourOfClubs = $"{PathToCards}cardClubs4";
+        public const string FiveOfClubs = $"{PathToCards}cardClubs5";
+        public const string SixOfClubs = $"{PathToCards}cardClubs6";
+        public const string SevenOfClubs = $"{PathToCards}cardClubs7";
+        public const string EightOfClubs = $"{PathToCards}cardClubs8";
+        public const string NineOfClubs = $"{PathToCards}cardClubs9";
+        public const string TenOfClubs = $"{PathToCards}cardClubs10";
+        public const string JackOfClubs = $"{PathToCards}cardClubsJ";
+        public const string QueenOfClubs = $"{PathToCards}cardClubsQ";
+        public const string KingOfClubs = $"{PathToCards}cardClubsK";
+
+        // Diamonds
+        public const string AceOfDiamonds = $"{PathToCards}cardDiamondsA";
+        public const string TwoOfDiamonds = $"{PathToCards}cardDiamonds2";
+        public const string ThreeOfDiamonds = $"{PathToCards}cardDiamonds3";
+        public const string FourOfDiamonds = $"{PathToCards}cardDiamonds4";
+        public const string FiveOfDiamonds = $"{PathToCards}cardDiamonds5";
+        public const string SixOfDiamonds = $"{PathToCards}cardDiamonds6";
+        public const string SevenOfDiamonds = $"{PathToCards}cardDiamonds7";
+        public const string EightOfDiamonds = $"{PathToCards}cardDiamonds8";
+        public const string NineOfDiamonds = $"{PathToCards}cardDiamonds9";
+        public const string TenOfDiamonds = $"{PathToCards}cardDiamonds10";
+        public const string JackOfDiamonds = $"{PathToCards}cardDiamondsJ";
+        public const string QueenOfDiamonds = $"{PathToCards}cardDiamondsQ";
+        public const string KingOfDiamonds = $"{PathToCards}cardDiamondsK";
+
+        // Hearts
+        public const string AceOfHearts = $"{PathToCards}cardHeartsA";
+        public const string TwoOfHearts = $"{PathToCards}cardHearts2";
+        public const string ThreeOfHearts = $"{PathToCards}cardHearts3";
+        public const string FourOfHearts = $"{PathToCards}cardHearts4";
+        public const string FiveOfHearts = $"{PathToCards}cardHearts5";
+        public const string SixOfHearts = $"{PathToCards}cardHearts6";
+        public const string SevenOfHearts = $"{PathToCards}cardHearts7";
+        public const string EightOfHearts = $"{PathToCards}cardHearts8";
+        public const string NineOfHearts = $"{PathToCards}cardHearts9";
+        public const string TenOfHearts = $"{PathToCards}cardHearts10";
+        public const string JackOfHearts = $"{PathToCards}cardHeartsJ";
+        public const string QueenOfHearts = $"{PathToCards}cardHeartsQ";
+        public const string KingOfHearts = $"{PathToCards}cardHeartsK";
+
+        // Spades
+        public const string AceOfSpades = $"{PathToCards}cardSpadesA";
+        public const string TwoOfSpades = $"{PathToCards}cardSpades2";
+        public const string ThreeOfSpades = $"{PathToCards}cardSpades3";
+        public const string FourOfSpades = $"{PathToCards}cardSpades4";
+        public const string FiveOfSpades = $"{PathToCards}cardSpades5";
+        public const string SixOfSpades = $"{PathToCards}cardSpades6";
+        public const string SevenOfSpades = $"{PathToCards}cardSpades7";
+        public const string EightOfSpades = $"{PathToCards}cardSpades8";
+        public const string NineOfSpades = $"{PathToCards}cardSpades9";
+        public const string TenOfSpades = $"{PathToCards}cardSpades10";
+        public const string JackOfSpades = $"{PathToCards}cardSpadesJ";
+        public const string QueenOfSpades = $"{PathToCards}cardSpadesQ";
+        public const string KingOfSpades = $"{PathToCards}cardSpadesK";
+    }
+}
