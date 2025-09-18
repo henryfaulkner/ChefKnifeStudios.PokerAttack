49af58bf51d49d462acb8e3aeaf474125c54a04a
Henry Faulkner <hfaulkner27@gmail.com>
2025-09-17 21:14:11 -0400

    observablecollection and no tests

--- src/Client/ChefKnifeStudios.PokerAttack.Client.Shared/Components/LobbyList.razor.cs
+++ src/Client/ChefKnifeStudios.PokerAttack.Client.Shared/Components/LobbyList.razor.cs
@@ -1,17 +1,45 @@
 ﻿using ChefKnifeStudios.PokerAttack.Client.Shared.ViewModels;
 using Microsoft.AspNetCore.Components;
+using System.Collections.Specialized;
+using System.ComponentModel;
 
 namespace ChefKnifeStudios.PokerAttack.Client.Shared.Components;
 
-public partial class LobbyList : ComponentBase
+public partial class LobbyList : ComponentBase, IDisposable
 {
     [Inject] IApplicationViewModel ApplicationViewModel { get; set; } = null!;
     [Inject] ILobbyViewModel LobbyViewModel { get; set; } = null!;
+
+    readonly string[] _subscriptions =
+    [
+        nameof(LobbyViewModel.Lobbies),
+        nameof(LobbyViewModel.IsLoadingLobbies),
+    ];
 
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
         await LobbyViewModel.LoadLobbiesAsync();
+
+        LobbyViewModel.PropertyChanged += ViewModel_OnPropertyChanged;
+        LobbyViewModel.Lobbies.CollectionChanged += Lobbies_CollectionChanged;
+    }
+
+    private void Lobbies_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        LobbyViewModel.PropertyChanged -= ViewModel_OnPropertyChanged;
+        LobbyViewModel.Lobbies.CollectionChanged -= Lobbies_CollectionChanged;
+    }
+
+    void ViewModel_OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (_subscriptions.Contains(e.PropertyName) is false) return;
+        Task.Run(async () => await InvokeAsync(StateHasChanged));
     }
 
     void HandleJoinLobbyPressed(string gameId)
